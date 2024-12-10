0bcac3d031b41772d2723981bfde3b61e62fd39c
Sunny Kumar <sk202542@gmail.com>
2024-12-10 15:30:52 +0530

    add 1 question

--- script.js
+++ script.js
@@ -166,6 +166,59 @@
 
 
 
+// Reverse a sentence without inbuilt
+
+// function reverseSentence(sentence) {
+//     let reversed = ""; // To store the final reversed sentence
+
+//     let word = ""; // To accumulate the current word
+    
+//     for (let i = sentence.length - 1; i >= 0; i--) {
+//         if (sentence.charAt(i) === " ") { // If space is found, add the word to the result
+//             if (word) { // Add only if the current word is not empty
+//                 reversed = reversed ? word + " " + reversed : word;
+//                 word = ""; // Reset the word
+//             }
+//         } else {
+//             word = sentence.charAt(i) + word; // Prepend the character to the current word
+//         }
+//     }
+    
+//     if (word) { // Add the last word if it exists
+//         reversed = reversed ? word + " " + reversed : word;
+//     }
+    
+//     return reversed.trim(); // Ensure no leading or trailing spaces
+// }
+
+// Example usage
+
+
+const sentence = "Hello    World from    JavaScript";
+sentence.trim();
+let wordarr= sentence.split(" ")
+
+let res = "";
+for(let i = wordarr.length-1 ; i>=0 ; i--){
+    if(wordarr[i]!==""){
+        res += wordarr[i]+" ";
+    }
+
+}
+console.log(res);
+
+
+ 
+
+
+// let res = sentence.split(" ").reverse().join(" ");
+//   console.log(res.trim());
+
+  
+
+
+
+
 
 
 
