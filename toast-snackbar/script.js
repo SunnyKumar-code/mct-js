99d4008cb9a2e122d89e9b91d80a759f065bb443
Sunny Kumar <sk202542@gmail.com>
2024-12-16 14:25:51 +0530

    make toster

--- toast-snackbar/script.js
+++ toast-snackbar/script.js
@@ -1,79 +1,68 @@
 let side = document.querySelector("#side");
-let side2 =document.querySelector("#side2");
+let side2 = document.querySelector("#side2");
 let info = document.querySelector("#info");
-let  mass = document.querySelector('#mass');
+let mass = document.querySelector("#mass");
 let duration = document.querySelector("#duration");
 let durOut = document.querySelector("#duration-output");
-let tosterDiv = document.querySelector("#toster-container");
-
-
+let tosterDiv = document.querySelector(".toster-container");
 let submit = document.querySelector(".btn");
-submit.addEventListener("click",()=>{
-   
-    setInterval(()=>{
-        if(side.value === "top" && side2.value ==="left" ) {
-       
-            if(info.value==="normal"){
-    
-    
-            }else if(info.value==="error"){
-    
-            }else if(info.value==="success"){
-    
-            }else if(info.value==="warning"){
-    
-            }else if(info.value==="info"){
-    
-            }
-    
-    
-        } else if(side.value === "top" && side2.value ==="right")  {
-            if(info.value==="normal"){
-    
-    
-            }else if(info.value==="error"){
-    
-            }else if(info.value==="success"){
-    
-            }else if(info.value==="warning"){
-    
-            }else if(info.value==="info"){
-    
-            }
-    
-        }else if(side.value === "bottom" && side2.value ==="left"){
-            if(info.value==="normal"){
-    
-    
-            }else if(info.value==="error"){
-    
-            }else if(info.value==="success"){
-    
-            }else if(info.value==="warning"){
-    
-            }else if(info.value==="info"){
-    
-            }
-    
-        }else if(side.value === "bottom" && side2.value ==="right"){
-            if(info.value==="normal"){
-    
-    
-            }else if(info.value==="error"){
-    
-            }else if(info.value==="success"){
-    
-            }else if(info.value==="warning"){
-    
-            }else if(info.value==="info"){
-    
-            }
-    
-        }
-    },duration.value*1000);
-    clearInterval()
-  
-})
-function showVal(value){
-    durOut.innerHTML =value;
-}
+
+submit.addEventListener("click", () => {
+    tosterDiv.className = `${side2.value}-${side.value}`;
+    let message = mass.value.trim();
+    if (!message) {
+        alert("Please enter a message!");
+        return;
+    }
+    let information = info.value;
+    tosterDiv.innerHTML = `<p>${message}</p> <span id="crose">x</span>`;
+    switch (information) {
+        case "normal":
+            tosterDiv.style.color = "black";
+            tosterDiv.style.display="flex";
+            tosterDiv.style.gap="10px";
+            tosterDiv.style.padding="10px";
+            tosterDiv.style.border = "1px solid black";
+            break;
+        case "error":
+            tosterDiv.style.display="flex";
+            tosterDiv.style.color = "red";
+            tosterDiv.style.gap="10px";
+            tosterDiv.style.padding="10px";
+            tosterDiv.style.border="1px solid red"
+            break;
+        case "success":
+            tosterDiv.style.display="flex";
+            tosterDiv.style.color = "green";
+            tosterDiv.style.gap="10px";
+            tosterDiv.style.padding="10px";
+            tosterDiv.style.border="1px solid green"
+            break;
+        case "warning":
+            tosterDiv.style.display="flex";
+            tosterDiv.style.color = "yellow";
+            tosterDiv.style.gap="10px";
+            tosterDiv.style.padding="10px";
+            tosterDiv.style.border="1px solid yellow"
+            break;
+        case "info":
+            tosterDiv.style.display="flex";
+            tosterDiv.style.color = "blue";
+            tosterDiv.style.gap="10px";
+            tosterDiv.style.padding="10px";
+            tosterDiv.style.border="1px solid blue"
+            break;
+    }
+
+    document.querySelector("#crose").addEventListener("click", () => {
+        tosterDiv.style.display="none";
+    });
+    let delay = duration.value * 1000;
+    setTimeout(() => {
+        tosterDiv.style.display="none";
+    }, delay);
+});
+
+duration.addEventListener("input", () => {
+    durOut.innerHTML = duration.value;
+});
